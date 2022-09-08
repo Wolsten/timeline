3792b33087cbd24abdfed3e8115f5490e112b47f
Stephen Davison <stephenjohndavison@gmail.com>
2022-09-08 17:59:01 +0100

    removed commented code

--- src/lib/timeline/Timeline.svelte
+++ src/lib/timeline/Timeline.svelte
@@ -134,9 +134,6 @@
 
     function handleClick() {
         // console.error('Handling timeline click in dataset',data.name)
-        // if (options.selectedEvent !== false) {
-        //     options.selectedEvent = false
-        // }
         if (options.selectedPoint !== false) {
             options.selectedPoint = false
         }
